c831547613b2a9fad723b6132c544b17acffd7f7
Aarthif-Nawaz <mohammed.2017313@iit.ac.lk>
2021-05-25 22:38:19 -0800

    New Changes

---
+++ build/precache-manifest.2921e044e4fbb0d36d0dd44f84abe80f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b513b3433d74e0b55903296725811dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248d9a83aec6e2fa34a4",
+    "url": "/static/css/2.1ca73698.chunk.css"
+  },
+  {
+    "revision": "d7d7214be6c621b4ddd1",
+    "url": "/static/css/main.607c8195.chunk.css"
+  },
+  {
+    "revision": "248d9a83aec6e2fa34a4",
+    "url": "/static/js/2.259dbe84.chunk.js"
+  },
+  {
+    "revision": "bca72865f3062b09afeed26d355ae136",
+    "url": "/static/js/2.259dbe84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d7214be6c621b4ddd1",
+    "url": "/static/js/main.fe91c499.chunk.js"
+  },
+  {
+    "revision": "7f7b4a838494c35a4266",
+    "url": "/static/js/runtime-main.efd31140.js"
+  },
+  {
+    "revision": "a06a1ee07cb7510c0078f4cc05f94225",
+    "url": "/static/media/10.a06a1ee0.jpg"
+  },
+  {
+    "revision": "9128c8ac1502c319c0c6ae998a035fc0",
+    "url": "/static/media/13.9128c8ac.jpg"
+  },
+  {
+    "revision": "d6f78bb1373a708c3f210ec49af1313c",
+    "url": "/static/media/2.d6f78bb1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8a422611d0bd4451914900a50d4237d5",
+    "url": "/static/media/icon-minus-gray.8a422611.svg"
+  },
+  {
+    "revision": "1f73c31fc5c9cbe5874f24c5b870b1de",
+    "url": "/static/media/icon-plus-gray.1f73c31f.svg"
+  },
+  {
+    "revision": "596814caa4fbaecbf5014bcfe8e363fb",
+    "url": "/static/media/simple-Line-Icons.596814ca.ttf"
+  },
+  {
+    "revision": "c9c9b5ebc6395eeb83072bf18e3818aa",
+    "url": "/static/media/simple-Line-Icons.c9c9b5eb.svg"
+  },
+  {
+    "revision": "f19a7f6c7a0b54b748277c40d7cf8882",
+    "url": "/static/media/simple-Line-Icons.f19a7f6c.eot"
+  },
+  {
+    "revision": "ff94ad94c3a9d04bd2f80cb3c87dcccb",
+    "url": "/static/media/simple-Line-Icons.ff94ad94.woff"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
